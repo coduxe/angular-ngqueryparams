48da4f56de6254def695abfe86d2b9e1e1da4350
oliver <grisha_91_@hotmail.com>
2016-06-10 10:57:32 +0100

    added angular-ngqueryparams

---
+++ src/angular-ngqueryparams.js
@@ -0,0 +1,43 @@
+/**
+* angular-ngqueryparams
+* @version v1.0.0
+* @link https://github.com/coduxe/angular-ngqueryparams
+* @author Coduxe, https://github.com/coduxe
+* @license MIT License, http://www.opensource.org/licenses/MIT
+*/
+
+'use strict';
+
+if (!angular) throw new TypeError("AngularJs is required");
+
+angular.module("angular-ngqueryparams", [])
+
+.provider('ngQueryParams', function() {
+
+  this.$get = function() {
+    var search = window.location.search;
+    var params = search ? search.substring(1).split("&").reduce(function(result, item) {
+      var split = item.split("=");
+      if (split[0].slice(-2) === '[]') {
+        var key = split[0].replace('[]', '');
+        result[key] ? result[key].push(split[1]) : result[key] = [split[1]];
+      }
+      else {
+        result[split[0]] = split[1];
+      }
+      return result;
+    }, {}) : {};
+
+    return {
+      all: function() {
+        return params;
+      },
+      get: function(attr) {
+        return params[attr];
+      },
+      exists: function(attr) {
+        return (typeof params[attr] !== 'undefined');
+      }
+    };
+  }
+});
